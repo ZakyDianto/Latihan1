d90967629e07e7463d169b0622cd7f41c9559c3c
ZakyDianto <140376583+ZakyDianto@users.noreply.github.com>
2024-09-09 10:38:06 +0700

    Location fix

--- proses_login.php
+++ proses_login.php
@@ -3,10 +3,10 @@ if($_POST){
     $username=$_POST['username'];
     $password=$_POST['password'];
     if (empty($username)) {
-        echo "<script>alert('Username tidak boleh kosong'); location.href='login.php';</script>";
+        echo "<script>alert('Username tidak boleh kosong'); location.href='index.php';</script>";
     }
     elseif (empty($password)) {
-        echo "<script>alert('Password tidak boleh kosong'); location.href='login.php';</script>";
+        echo "<script>alert('Password tidak boleh kosong'); location.href='index.php';</script>";
     }
     else{
         include 'koneksi.php';
@@ -14,14 +14,14 @@ if($_POST){
     }
         if (mysqli_num_rows($qry_login)>0) {
            $dt_login=mysqli_fetch_array($qry_login);
-           session_start();
+           session_start(); 
            $_SESSION['id_pegawai']=$dt_login['id_pegawai'];
            $_SESSION['nama']=$dt_login['nama'];
            $_SESSION['status_login']=true;
            header("location: tampil_pegawai.php");
         }
         else{
-            echo "<script>alert('Username dan Password tidak benar');location.href='login.php';</script>";
+            echo "<script>alert('Username dan Password tidak benar');location.href='index.php';</script>";
         }
 }
 ?>
